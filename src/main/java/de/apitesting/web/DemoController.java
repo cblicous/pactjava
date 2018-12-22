e5a43c568f6a2b71a5a32bcd3cf3b9aad6bc1472
Christian Belka <Christian.Belka@mgm-tp.com>
2018-12-22 18:37:11 +0100

    Further dev

--- src/main/java/de/apitesting/web/DemoController.java
+++ src/main/java/de/apitesting/web/DemoController.java
@@ -1,16 +1,17 @@
-package de.apitesting;
+package de.apitesting.web;
 
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 
-@Controller
+@RestController
 public class DemoController {
 
-    @RequestMapping("/")
+    @RequestMapping("/test")
         public String welcome() {
-            return "index";
+            return "test";
         }
 
 }
