25c71ecefb42c86683cb9cb61ff5f8eabdfd7537
Christian Belka <Christian.Belka@mgm-tp.com>
2018-12-22 18:28:18 +0100

    Initial app for testing pact

---
+++ src/main/java/de/apitesting/DemoController.java
@@ -0,0 +1,16 @@
+package de.apitesting;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+
+@Controller
+public class DemoController {
+
+    @RequestMapping("/")
+        public String welcome() {
+            return "index";
+        }
+
+}
